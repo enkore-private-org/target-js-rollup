959919423aa49a43d516775b17b805bd9a004c9b
eXory2024 <exory@anio.software>
2024-10-17 22:21:53 +0200

    Export ts.

--- src/index.mjs
+++ src/index.mjs
@@ -2,5 +2,7 @@ export {tsStripTypesFromCode} from "./ts/stripTypesFromCode.mjs"
 export {tsReadTSConfigFile} from "./ts/readTSConfigFile.mjs"
 export {tsInvokeTypeScript} from "./ts/invokeTypeScript.mjs"
 
+export * as ts from "typescript"
+
 export {jsBundler} from "./js/bundler.mjs"
 export {jsResolveImportAliases} from "./js/resolveImportAliases.mjs"
